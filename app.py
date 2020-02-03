059f3186913bafa4d9a2d564afd0278ae6bdc30f
GroverW <cheesemaker@gmail.com>
2020-02-03 12:05:54 -0800

    Added GET routes for cupcakes and individual cupcakes. Added POST route to add new cupcakes.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Flask app for Cupcakes"""
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models import db, connect_db, Cupcake
+from serializer import serialize_cupcake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///cupcakes"
@@ -13,8 +14,46 @@ db.create_all()
 
 app.config['SECRET_KEY'] = 'apoisdhfpaiosdhfpioasd'
 
+
 @app.route('/api/cupcakes')
 def get_cupcakes():
     cupcakes = Cupcake.query.all()
+    serialized = [Cupcake.serialize(c) for c in cupcakes]
+
+    return (jsonify(cupcakes=serialized), 200)
+
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    cupcake = Cupcake.query.get(id)
+
+    if cupcake:
+        serialized = Cupcake.serialize(cupcake)
+        return (jsonify(cupcake=serialized), 200)
+    else:
+        return (jsonify(error="Cupcake doesn't exist."), 404)
+
+
+@app.route('/api/cupcakes', methods=['POST'])
+def add_cupcake():
+    flavor = request.json.get('flavor')
+    size = request.json.get('size')
+    rating = request.json.get('rating')
+    image = request.json.get('image') or None
+
+    new_cupcake = Cupcake(
+                        flavor=flavor,
+                        size=size,
+                        rating=rating,
+                        image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = Cupcake.serialize(new_cupcake)
+
+    return (jsonify(cupcake=serialized), 201)
+
+    
 
     
--- models.py
+++ models.py
@@ -10,10 +10,20 @@ class Cupcake(db.Model):
     __tablename__ = 'cupcakes'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    flavor = db.Column(db.String(10), nullable=False)
-    size = db.Column(db.String(10), nullable=False)
+    flavor = db.Column(db.String(24), nullable=False)
+    size = db.Column(db.String(24), nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake")
+
+    @classmethod
+    def serialize(self, cupcake):
+        return {
+            'id': cupcake.id,
+            'flavor': cupcake.flavor,
+            'size': cupcake.size,
+            'rating': cupcake.rating,
+            'image': cupcake.image
+        }
 
 
 def connect_db(app):
