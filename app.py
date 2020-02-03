90e7b2461d51168ca8f63d557bbfcfa0f4d333c9
GroverW <cheesemaker@gmail.com>
2020-02-03 11:18:18 -0800

    Setup cupcake model, app configs and cupcake serializer.

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+
+app.config['SECRET_KEY'] = 'apoisdhfpaiosdhfpioasd'
+
+@app.route('/api/cupcakes')
+def get_cupcakes():
+    cupcakes = Cupcake.query.all()
+
+    
--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cupcake(db.Model):
+    " Cupcake "
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(10), nullable=False)
+    size = db.Column(db.String(10), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake")
+
+
+def connect_db(app):
+    " Connect this database to provided flask app. "
+
+    db.app = app
+    db.init_app(app)
